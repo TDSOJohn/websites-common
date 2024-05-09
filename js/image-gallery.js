424c1d5410a47720354ca01f4cd240963231fca2
ARtbibaOfficial <giovanni.basso@icloud.com>
2024-05-09 20:16:03 +0200

    improved fullscreen image layout

--- js/image-gallery.js
+++ js/image-gallery.js
@@ -3,6 +3,8 @@ var host_head;
 
 var fullscreen_element;
 
+// change to "" for testing
+var path_to_common = "common/"
 
 // populate variables once the page is loaded
 window.onload = (event) => {
@@ -27,17 +29,17 @@ function close_fullscreen() {
 function load_fullscreen(src) {
     // request image-full-screen component
     var xhr = new XMLHttpRequest();
-    xhr.open('GET', 'common/components/image-full-screen.html', true);
+    xhr.open('GET', 'components/image-full-screen.html', true);
     xhr.onreadystatechange = function() {
         if (this.readyState !== 4) return;
         if (this.status !== 200) return; // or whatever error handling you want
         // add head to main head
         var doc = new DOMParser().parseFromString(this.responseText, 'text/html');
-        host_head.innerHTML += doc.head.innerHTML;
+        host_head.innerHTML += '<link rel="stylesheet" type="text/css" href="' + path_to_common + 'css/image-full-screen.css' + '">';
         // add component body to full-screen-image-holder
-        document.getElementById('full-screen-image-holder').innerHTML = doc.body.innerHTML;
+        fullscreen_element = document.getElementById('full-screen-image-holder');
+        fullscreen_element.innerHTML = doc.body.innerHTML;
         document.getElementById("image-full-screen").src = src;
-        fullscreen_element = document.getElementById('image-full-screen-component');
         fullscreen_element.addEventListener("click", function() {
             close_fullscreen();
         });
